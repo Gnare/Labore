e22949d33444ea9753cb3c87b34069b020648c95
GNARE CORP <nilolive@gmail.com>
2019-08-10 15:58:17 -0300

    Deletei tudo

--- server/src/test/resources/logback.groovy
+++
@@ -1,9 +0,0 @@
-appender("CONSOLE", ConsoleAppender) {
-  encoder(PatternLayoutEncoder) {
-    pattern = "%d{HH:mm:ss.SSS} [%thread] %-5level %logger{36} - %msg%n"
-  }
-}
-
-logger("net.fender.springboot.aws.cloudsearch", DEBUG)
-
-root(INFO, ["CONSOLE"])
